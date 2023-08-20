f1ffd7ff2928dee33b47459e2b6ae80fe12641d4
Ketan Sangle <ksangle24@gmail.com>
2023-08-20 14:30:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/githubapi/ui/PullRequestPage.kt
@@ -0,0 +1,9 @@
+package com.example.githubapi.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PullRequestPage() {
+    return Text("This will show PR for a specific repository")
+}
